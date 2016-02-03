a908db885245973d7f62870b569adaedeb48b88c
Daniel Favand <Daniel Favand>
2016-02-03 00:38:51 -0500

    working socket relay

---
+++ config.js
@@ -0,0 +1,7 @@
+/*global process*/
+var config = {};
+
+config.ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+config.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+module.exports = config;
